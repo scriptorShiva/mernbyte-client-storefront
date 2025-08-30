8d0a6fa5dd28ecb6c861ee806be3bec91271acb0
scriptorshiva <shivapal108941@gmail.com>
2025-08-30 23:58:25 +0530

    Static : hero section layout

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,9 +4,30 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <>
-      <div>I am the home page</div>
-      <p>Hello, Next.js!</p>
-      <Button>Click me</Button>
+      <section className="bg-white">
+        <div className="container mx-auto flex items-center justify-between px-[50px]">
+          {/* left */}
+          <div>
+            <h1 className="text-7xl font-black font-sans ">
+              Everything You Need <br />{" "}
+              <span className="text-primary">Around the Corner</span>
+            </h1>
+            <p className="text-2xl mt-8 max-w-lg leading-sung">
+              Support nearby sellers and get what you want, whenever you want.
+            </p>
+            <Button
+              className="mt-8 text-lg rounded-full py-7 px-6 font-bold cursor-pointer"
+              size={"lg"}
+            >
+              Shop Nearby
+            </Button>
+          </div>
+          {/* right */}
+          <div>
+            <Image width={"700"} height={"700"} src={"/hero.png"} alt="hero" />
+          </div>
+        </div>
+      </section>
     </>
   );
 }
--- src/components/custom/Navbar/index.tsx
+++ src/components/custom/Navbar/index.tsx
@@ -1,3 +1,5 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
 import {
   Select,
   SelectContent,
@@ -5,13 +7,22 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select";
+import { MapPinHouse, ShoppingBasket } from "lucide-react";
+import Link from "next/link";
 
 const Navbar = () => {
   return (
     <header className="bg-white shadow">
       <nav className="container mx-auto flex items-center justify-between py-4 px-[100px]">
+        {/* left side */}
         <div className="flex items-center">
-          <img src="/logo.png" alt="Logo" className="h-10 w-auto" />
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            className="h-10 w-auto"
+            width={"100"}
+            height={"100"}
+          />
           <span className="font-bold text-gray-600 ">localmart</span>
           <div className="px-4">
             <Select>
@@ -25,6 +36,38 @@ const Navbar = () => {
               </SelectContent>
             </Select>
           </div>
+        </div>
+        {/* right side */}
+        <div>
+          <ul className="flex space-x-8">
+            <li>
+              <Link className="hover:text-primary" href={"/"}>
+                Menu
+              </Link>
+            </li>
+            <li>
+              <Link className="hover:text-primary" href={"/"}>
+                Orders
+              </Link>
+            </li>
+            <li className="relative">
+              <Link className="hover:text-primary" href={"/cart"}>
+                <ShoppingBasket />
+              </Link>
+              <span className="absolute -top-4 -right-4 h-6 bg-primary rounded-full text-white font-bold p-2 flex items-center justify-center text[2px]">
+                4
+              </span>
+            </li>
+            <li>
+              <Link href={"/"} className="flex space-x-2 hover:text-primary">
+                <MapPinHouse />
+                <span> North East, Delhi</span>
+              </Link>
+            </li>
+            <li>
+              <Button size={"sm"}>Logout</Button>
+            </li>
+          </ul>
         </div>
       </nav>
     </header>
